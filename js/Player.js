0ec61671fc094ec0e17cf63360a174fc4b846e04
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-12-27 16:58:32 -0300

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,89 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+    this.rank = 0;
+    this.fuel = 160;
+    //é assim que se adiciona a propriedade score
+    this.score = 0;
+    //agora, adicione a propriedade life
+
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    database.ref(playerIndex).set({
+      name: this.name,
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      //é assim que escreve o valor de score no banco de dados
+      score: this.score,  
+      //agora, escreva o novo valor de life
+
+    });
+  }
+
+  getDistance() {
+    var playerDistanceRef = database.ref("players/player" + this.index);
+    playerDistanceRef.on("value", data => {
+      var data = data.val();
+      this.positionX = data.positionX;
+      this.positionY = data.positionY;
+    });
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", data => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).update({
+      positionX: this.positionX,
+      positionY: this.positionY,
+      rank: this.rank,
+      //é assim que escreve o valor de score no banco de dados
+      score: this.score,  
+      //agora, escreva o novo valor de life
+
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+
+  getCarsAtEnd() {
+    database.ref("carsAtEnd").on("value", data => {
+      this.rank = data.val();
+    });
+  }
+
+  static updateCarsAtEnd(rank) {
+    database.ref("/").update({
+      carsAtEnd: rank
+    });
+  }
+}
